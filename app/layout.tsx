ff9b881a48b821e0b24d482bc7a4e2aff36ab1fd
yats1304 <chaubaly@gmail.com>
2024-12-17 20:44:17 +0530

    project done

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+         {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
